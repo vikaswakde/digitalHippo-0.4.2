8e3c6a6dbbd709abddd0eb91276ebb082920ceda
Vikas Wakde <110342308+vikaswakde@users.noreply.github.com>
2024-05-25 22:50:22 +0530

    'image dispaly issue fix 2'

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "3000",
         protocol: "http",
       },
+      {
+        hostname: "favourite-pearle-vikasorg-1af1b74b.koyeb.app",
+        pathname: "**",
+        port: "*",
+        protocol: "https",
+      },
     ],
   },
 };
